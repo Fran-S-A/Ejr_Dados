c287e93b2d4a22d2609b5cf6f181162bcba8afd8
Francisco Sierra Ares <franciscosa98@educastur.es>
2024-04-16 00:33:02 +0200

    Variables refactorizadas del código

--- src/juego/JuegoDados.java
+++ src/juego/JuegoDados.java
@@ -10,7 +10,7 @@ public class JuegoDados {
 		int valorDado1 = 0;
         int valorDado2 = 0;
         Random dado = new Random(); // Random que se utiliza para generar los valores.
-        int contador = 0; // Contador para las tiradas
+        int tiradas = 0; // Contador para las tiradas
         /**
          * Se utiliza un bucle para realizar tiradas hasta que ambos valores sean 6.
          */
@@ -23,10 +23,10 @@ public class JuegoDados {
 	        valorDado2 = dado.nextInt(6)+1;
 	        System.out.println( "Dado 1 - Dado 2");
 	        System.out.println("  "+valorDado1+" 	   "+valorDado2);
-	        contador++;
+	        tiradas++;
         }
         // Al terminar se imprimen las tiradas.
-        System.out.println("Número de tiradas: " + contador);
+        System.out.println("Número de tiradas: " + tiradas);
 	}
 
 }
