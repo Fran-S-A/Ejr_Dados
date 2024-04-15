7a8c367103daab06166eba680e36a4be7c85b2d6
Francisco Sierra Ares <franciscosa98@educastur.es>
2024-04-16 00:45:36 +0200

    Se añade el método mostrarResultado. Se corrigen los comentarios.

--- src/juego/JuegoDados.java
+++ src/juego/JuegoDados.java
@@ -15,20 +15,29 @@ public class JuegoDados {
          * Se utiliza un bucle para realizar tiradas hasta que ambos valores sean 6.
          */
         while (valorDado1 != 6 || valorDado2 != 6){
-	        /**
-	         * Se genera el valor entre 0 y el valor especificado, por esto se suma 1,
-	         * para que nunca salga 0. Además se imprimen los resultados.
-	         */
 	        valorDado1 = tirada(dado);
 	        valorDado2 = tirada(dado);
-	        System.out.println( "Dado 1 - Dado 2");
-	        System.out.println("  "+valorDado1+" 	   "+valorDado2);
+	        mostrarResultados(valorDado1, valorDado2);
 	        tiradas++;
         }
         // Al terminar se imprimen las tiradas.
         System.out.println("Número de tiradas: " + tiradas);
 	}
-
+	/**
+	 * Método que imprime los resultados de las tiradas.
+	 * @param valorDado1 Entero con el valor del dado 1.
+	 * @param valorDado2 Entero con el valor del dado 2.
+	 */
+	public static void mostrarResultados(int valorDado1, int valorDado2) {
+		System.out.println( "Dado 1 - Dado 2");
+		System.out.println("  "+valorDado1+" 	   "+valorDado2);
+	}
+	/**
+	 * Método que genera el valor entre 0 y el valor especificado, por esto se suma 1,
+     * para que nunca salga 0.
+	 * @param dado Objeto Random
+	 * @return Valor del dado
+	 */
 	public static int tirada(Random dado) {
 		return dado.nextInt(6)+1;
 	}
