b1033cf6c2ca1c8d327b4511c33436e50c4f1f8c
Francisco Sierra Ares <francioscosa98@educastur.es>
2024-04-15 23:11:54 +0200

    Proyecto sin cambios. Versión 0

---
+++ src/juego/JuegoDados.java
@@ -0,0 +1,22 @@
+package juego;
+import java.util.Random;
+
+public class JuegoDados {
+
+	public static void main(String[] args) {
+		int valorDado1 = 0;
+        int valorDado2 = 0;
+        Random dado = new Random();
+        int contador = 0;
+        while (valorDado1 != 6 || valorDado2 != 6){
+	        
+	        valorDado1 = dado.nextInt(6)+1;
+	        valorDado2 = dado.nextInt(6)+1;
+	        System.out.println( "Dado 1 - Dado 2");
+	        System.out.println("  "+valorDado1+" 	   "+valorDado2);
+	        contador++;
+        }
+        System.out.println("Número de tiradas: " + contador);
+	}
+
+}
